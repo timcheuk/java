b3d805532a12d3a82a83ac6fa45d19fc0f3fa8ca
timcheuk <77771021+timcheuk@users.noreply.github.com>
2021-02-08 11:46:19 +0800

    db and remove extra code
    add database file and revert some extra code for role base

--- springsecurity/src/main/java/com/timhappyjava/springsecurity/User.java
+++ springsecurity/src/main/java/com/timhappyjava/springsecurity/User.java
@@ -1,18 +1,10 @@
 package com.timhappyjava.springsecurity;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 
@@ -27,13 +19,6 @@ public class User {
 	private String password;
 	private boolean enabled;
 	
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinTable(
-			name = "user_role",
-			joinColumns = @JoinColumn(name="user_id"),
-			inverseJoinColumns = @JoinColumn(name="role_id")
-			)
-	private Set<Role> roles = new HashSet<>();
 	
 	public long getId() {
 		return id;
@@ -58,12 +43,6 @@ public class User {
 	}
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
-	}
-	public Set<Role> getRoles() {
-		return roles;
-	}
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
 	}
 	
 	
